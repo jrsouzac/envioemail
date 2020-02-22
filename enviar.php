a7ccc009b4c9bf212e21342a260a3deb58348d58
jrsouzac <fabriciojuniorsc@gmail.com>
2020-02-22 11:08:35 -0300

    Primeiro commit

---
+++ enviar.php
@@ -0,0 +1,29 @@
+
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer-master\PHPMailer-master\src\Exception.php';
+require 'PHPMailer-master\PHPMailer-master\src\PHPMailer.php';
+require 'PHPMailer-master\PHPMailer-master\src\SMTP.php';
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPSecure = 'ssl';
+$mail->SMTPAuth = true;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 465;
+$mail->Username = 'fabriciojuniorsc@gmail.com';
+$mail->Password = 'senha';
+$mail->setFrom('fabriciojuniorsc@gmail.com');
+$mail->addAddress($_POST['dest']);
+$mail->Subject = $_POST['ass'];
+$mail->Body = $_POST['msg'];
+//send the message, check for errors
+if($mail->send()){
+    echo 'Email enviado com sucesso';
+} else{
+    echo $mail->ErrorInfo;
+}
+
+?>
